cf9a437387248de66477a37acc2955162e8bdf7f
Tomsoya8810 <108568019+Tomsoya8810@users.noreply.github.com>
2024-06-26 13:47:46 +0900

    task6, 7を提出します

---
+++ task6/task2.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    // イベント発火時の処理
+    const setString = (event) => {
+        let product;
+        switch (event.record.サイボウズ製品.value) {
+            case "kintone":
+                product = "KN";
+                break; 
+            case "Garoon":
+                product = "GR";
+                break;
+            case "サイボウズ Office":
+                product = "OF";
+                break;
+            default:
+                product = "MW";
+                break; 
+        }
+        event.record.重複禁止項目_文字列.value = `${dateFns.format(event.record.日付.value, "yyyyMMdd")}-${product}-${event.record.管理番号.value}`;
+    }
+
+    // イベントの一括登録
+    const fields = ["日付", "サイボウズ製品", "管理番号"]
+    const targetEvents = [
+        "app.record.edit.show",
+        ...fields.map((field) => `app.record.create.change.${field}`),
+        ...fields.map((field) => `app.record.edit.change.${field}`),
+    ]
+    kintone.events.on(targetEvents, (event) => {
+        setString(event);
+        return event;
+    })
+})();
