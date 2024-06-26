f22ab6e23ec40907d3c8b4d2d5f0f8639c08b271
Tomsoya8810 <108568019+Tomsoya8810@users.noreply.github.com>
2024-06-26 12:45:25 +0900

    table要素への変更、クエリの

--- task5/index.js
+++ task5/index.js
@@ -1,44 +1,41 @@
-const outputData = (array) => {
-    const sortedArray = array.sort((a, b) => new Date(b.day.value) - new Date(a.day.value))
-    sortedArray.forEach((data) => {
-        const day = document.createElement("p");
-        const category = document.createElement("p");
+const outputData = (dataArray) => {
+    const table = document.createElement("table");
+    table.style.border = "1px solid black"
+    dataArray.forEach((data) => {
+        const row = document.createElement("tr");
+        const dayTd = document.createElement("td");
+        const categoryTd = document.createElement("td");
+        const titleTd = document.createElement("td");
         const link = document.createElement("a");
-        const tableRow = document.createElement("div");
-        day.textContent = data.day.value;
-        category.textContent = data.category.value;
+        dayTd.textContent = data.day.value;
+        categoryTd.textContent = data.category.value;
         link.textContent = data.content.value.length > 30 ? data.content.value.substring(0, 30) + "..." : data.content.value;
         link.href = data.url.value;
         link.target = data.target.value;
-        tableRow.classList.add("table-row");
-        [day, category, link].forEach((ele, i) => {
-            const tableBox = document.createElement("div");
-            tableBox.classList.add(`table-box`);
-            tableBox.classList.add(`col${i + 1}`);
-            tableBox.appendChild(ele);
-            tableRow.appendChild(tableBox);
-            if (i === 1) {
-                if (data.category.value === "製品") {
-                    tableBox.style.backgroundColor = "lightgreen";
-                } else if (data.category.value === "IR 情報") {
-                    tableBox.style.backgroundColor = "pink";
-                } else {
-                    tableBox.style.backgroundColor = "lightblue";
-                }
-            }
-        });
-        document.getElementById("table").appendChild(tableRow);
+        if (data.category.value === "製品") {
+            categoryTd.style.backgroundColor = "lightgreen";
+        } else if (data.category.value === "IR 情報") {
+            categoryTd.style.backgroundColor = "pink";
+        } else {
+            categoryTd.style.backgroundColor = "lightblue";
+        }
+        [dayTd, categoryTd, titleTd].forEach(ele => {ele.style.border = "1px solid black"})
+
+        titleTd.appendChild(link);
+        row.append(dayTd, categoryTd, titleTd);
+        table.append(row)
     })
+    document.getElementById("table").appendChild(table);   
 }
 
 axios.get("https://oykh3vmu623yt5rufrstzlyxyi0kitod.lambda-url.ap-northeast-1.on.aws/", {
     params: {
         id: "dojo",
-        query: "limit 3",
+        query: "order by day desc limit 3",
     }
 })
 .then(
     (res) => {outputData(res.data)}
 ).catch(
-    () => window.alert("情報を取得できませんでした")
+    () => window.alert("情報を取得できませんでした。")
 )
