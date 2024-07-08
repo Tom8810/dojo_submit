1d2816518673459e1fea7380ec4ae0cacf358824
Tomsoya8810 <108568019+Tomsoya8810@users.noreply.github.com>
2024-07-08 16:35:41 +0900

    修正しました

--- task9/task9.js
+++ task9/task9.js
@@ -1,19 +1,17 @@
 (() => {
     "use strict";
-    const body = {
-        app: 28,
-        fields: ["重複禁止項目", "レコード番号"]
-    }
-    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], async (event) => {
-        let returnValue = true;
-        const promises = [kintone.app.record.getId() ?? -1, kintone.api(kintone.api.url("/k/v1/records.json"), "GET", body)]
-        await Promise.all(promises)
-        .then(
-            ([id, recordsData]) => {
-            const duplicateCheckArray = recordsData.records.filter((record => Number(record.レコード番号.value) !== id)).map((record) => record.重複禁止項目.value);
-            if (duplicateCheckArray.includes(event.record.重複禁止項目.value)) {
-                if (!window.confirm("レコードが重複しています。このまま保存しますか？")) returnValue = false;
-            };
+    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], (event) => {
+        const body = {
+            app: 28,
+            query: `重複禁止項目 = "${event.record.重複禁止項目.value}" and レコード番号 != "${kintone.app.record.getId() ?? -1}"`,
+            totalCount: true
+        }
+        let returnValue = false;
+        kintone.api(kintone.api.url("/k/v1/records.json"), "GET", body).then(
+            (res) => {
+                if (res.totalCount) {
+                    if (!window.confirm("レコードが重複しています。このまま保存しますか？")) returnValue = false;
+                };
             }
         )
         return returnValue;
