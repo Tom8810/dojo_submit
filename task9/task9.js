f32e10df0c66836dd7570e49f2382b0f84089d2a
Tomsoya8810 <108568019+Tomsoya8810@users.noreply.github.com>
2024-07-09 10:02:49 +0900

    再度修正を行いました

--- task9/task9.js
+++ task9/task9.js
@@ -1,15 +1,15 @@
 (() => {
     "use strict";
-    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], (event) => {
+    kintone.events.on(["app.record.create.submit", "app.record.edit.submit"], async (event) => {
         const body = {
             app: 28,
             query: `重複禁止項目 = "${event.record.重複禁止項目.value}" and レコード番号 != "${kintone.app.record.getId() ?? -1}"`,
             totalCount: true
         }
-        let returnValue = false;
-        kintone.api(kintone.api.url("/k/v1/records.json"), "GET", body).then(
+        let returnValue = true;
+        await kintone.api(kintone.api.url("/k/v1/records.json"), "GET", body).then(
             (res) => {
-                if (res.totalCount) {
+                if (Number(res.totalCount)) {
                     if (!window.confirm("レコードが重複しています。このまま保存しますか？")) returnValue = false;
                 };
             }
